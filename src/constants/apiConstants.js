5237eb6e0f53ea5200816c96270d9d678c04b4e2
CARLOS HERNANDEZ <chernandez-27@outlook.com>
2026-04-26 11:11:52 -0600

    delete sources

--- src/constants/apiConstants.js
+++
@@ -1,5 +0,0 @@
-export const API_URL_PRODUCTION = 'https://uatbancaempresas.davivienda.com.sv/APIFinanciamientoEmpresas/api'
-export const API_URL_DEVELOP = 'http://localhost:8080/api'
-
-export const API_URL_PRODUCTION_AUTH = 'https://uatbancaempresas.davivienda.com.sv/APIFinanciamientoEmpresas'
-export const API_URL_DEVELOP_AUTH = 'http://localhost:8080'
