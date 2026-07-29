ec9b6c6590c226dc80bd397be0af35f5bc9a6143
CARLOS HERNANDEZ <chernandez-27@outlook.com>
2026-07-29 17:02:10 -0600

    :wastebasket: Delete: deprecated code

--- src/utils/getISOWeekOfMonth.js
+++
@@ -1,9 +0,0 @@
-const getISOWeekOfMonth = (date) => {
-    const fecha = new Date(date);
-    const dia = fecha.getDate();
-
-    const semana = Math.min(Math.ceil(dia / 7), 4); 
-
-    return semana;
-};
-export default getISOWeekOfMonth;
